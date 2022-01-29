6bcb4664b9d5548e95bc6520c6e20116359d5e17
generic-eric <98109426+generic-eric@users.noreply.github.com>
2022-01-29 01:18:42 -0600

    Tabs old and busted.  Spaces new hotness.

--- Source/IssaGame/attributesComponent.cpp
+++ Source/IssaGame/attributesComponent.cpp
@@ -6,74 +6,74 @@
 // Sets default values for this component's properties
 UattributesComponent::UattributesComponent()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = false;
+    // Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+    // off to improve performance if you don't need them.
+    PrimaryComponentTick.bCanEverTick = false;
 
-	// Set your defaults in blueprint
-	maxHealth = 0;
-	health = maxHealth;
-	maxLungCapacity = 0;
-	lungCapacity = maxLungCapacity;
-	isDead = true;
-	isCritical = true;
+    // Set your defaults in blueprint
+    maxHealth = 0;
+    health = maxHealth;
+    maxLungCapacity = 0;
+    lungCapacity = maxLungCapacity;
+    isDead = true;
+    isCritical = true;
 
-	// ...
+    // ...
 }
 
 
 int UattributesComponent::getHealth()
 {
-	return health;
+    return health;
 }
 
 int UattributesComponent::setHealth(int newHealthValue)
 {
-	if (newHealthValue > maxHealth)
-	{
-		newHealthValue = maxHealth;
-	}
-	else
-	{
-		health = newHealthValue;
-	}
+    if (newHealthValue > maxHealth)
+    {
+        newHealthValue = maxHealth;
+    }
+    else
+    {
+        health = newHealthValue;
+    }
 
-	return health;
+    return health;
 }
 
 int UattributesComponent::getLungCapacity()
 {
-	return lungCapacity;
+    return lungCapacity;
 }
 
 int UattributesComponent::setLungCapacity(int newLungCapacityValue)
 {
-	if (newLungCapacityValue > maxLungCapacity)
-	{
-		newLungCapacityValue = maxLungCapacity;
-	}
-	else
-	{
-		lungCapacity = newLungCapacityValue;
-	}
+    if (newLungCapacityValue > maxLungCapacity)
+    {
+        newLungCapacityValue = maxLungCapacity;
+    }
+    else
+    {
+        lungCapacity = newLungCapacityValue;
+    }
 
-	return lungCapacity;
+    return lungCapacity;
 }
 
 // Called when the game starts
 void UattributesComponent::BeginPlay()
 {
-	Super::BeginPlay();
+    Super::BeginPlay();
 
-	// ...
-	
+    // ...
+    
 }
 
 
 // Called every frame
 void UattributesComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+    // ...
 }
--- Source/IssaGame/attributesComponent.h
+++ Source/IssaGame/attributesComponent.h
@@ -10,45 +10,45 @@
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class ISSAGAME_API UattributesComponent : public UActorComponent
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
 
-public:	
-	// Sets default values for this component's properties
-	UattributesComponent();
+public:    
+    // Sets default values for this component's properties
+    UattributesComponent();
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-		int maxHealth;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+        int maxHealth;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-		int maxLungCapacity;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+        int maxLungCapacity;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-		bool isDead;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+        bool isDead;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-		bool isCritical;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+        bool isCritical;
 
-	UFUNCTION(BlueprintCallable)
-		int getHealth();
+    UFUNCTION(BlueprintCallable)
+        int getHealth();
 
-	UFUNCTION(BlueprintCallable)
-		int setHealth(int newHealthValue);
+    UFUNCTION(BlueprintCallable)
+        int setHealth(int newHealthValue);
 
-	UFUNCTION(BlueprintCallable)
-		int getLungCapacity();
+    UFUNCTION(BlueprintCallable)
+        int getLungCapacity();
 
-	UFUNCTION(BlueprintCallable)
-		int setLungCapacity(int newLungCapacityValue);
+    UFUNCTION(BlueprintCallable)
+        int setLungCapacity(int newLungCapacityValue);
 
 private:
-	int health;
-	int lungCapacity;
+    int health;
+    int lungCapacity;
 
 protected:
-	// Called when the game starts
-	virtual void BeginPlay() override;
+    // Called when the game starts
+    virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+public:    
+    // Called every frame
+    virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 };
